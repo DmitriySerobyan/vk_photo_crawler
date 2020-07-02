06aec170e4279d268282549109d1fe3e9e54be84
dmitriy serobyan <dmitriyserobyan@yandex.ru>
2020-07-02 23:13:49 +0500

    parallel photo download

--- src/main/kotlin/ru/serobyan/vk_photo_crawler/service/vk/VkPhotoDownloader.kt
+++ src/main/kotlin/ru/serobyan/vk_photo_crawler/service/vk/VkPhotoDownloader.kt
@@ -3,6 +3,11 @@ package ru.serobyan.vk_photo_crawler.service.vk
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.apache.Apache
 import io.ktor.client.request.get
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import org.apache.commons.io.FilenameUtils
 import org.jetbrains.exposed.sql.and
 import org.jetbrains.exposed.sql.transactions.transaction
@@ -13,15 +18,25 @@ import java.io.File
 import java.net.URL
 
 class VkPhotoDownloader(
-    private val photosDir: String = Config.photosDir
+    private val photosDir: String = Config.photosDir,
+    private val parallelPhotoDownloadCount: Int = Runtime.getRuntime().availableProcessors() * 4
 ) {
     suspend fun downloadPhotos() {
-        while (true) {
-            val vkPhotos = getVkPhotos()
-            if(vkPhotos.isEmpty()) break
-            vkPhotos.forEach { vkPhoto ->
-                savePhotoInFile(url = vkPhoto.photoUrl!!)
-                markDownloaded(vkPhoto)
+        runBlocking(Dispatchers.IO) {
+            val vkPhotos = produce {
+                while (true) {
+                    val vkPhotos = getVkPhotos()
+                    if (vkPhotos.isEmpty()) break
+                    vkPhotos.forEach { send(it) }
+                }
+            }
+            repeat(parallelPhotoDownloadCount) {
+                launch {
+                    for (vkPhoto in vkPhotos) {
+                        savePhotoInFile(url = vkPhoto.photoUrl!!)
+                        markDownloaded(vkPhoto)
+                    }
+                }
             }
         }
     }
@@ -30,7 +45,7 @@ class VkPhotoDownloader(
         return transaction {
             VkPhoto
                 .find { (VkPhotoTable.isDownloaded eq false) and (VkPhotoTable.photoUrl.isNotNull()) }
-                .limit(100)
+                .limit(1000)
                 .toList()
         }
     }
