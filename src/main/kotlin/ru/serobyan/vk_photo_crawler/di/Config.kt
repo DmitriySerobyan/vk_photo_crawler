fc6cbe8cd9ece950e8e8941612da51d3a2754fce
dmitriy serobyan <dmitriyserobyan@yandex.ru>
2020-08-01 20:35:10 +0500

    fix

--- src/main/kotlin/ru/serobyan/vk_photo_crawler/di/Config.kt
+++ src/main/kotlin/ru/serobyan/vk_photo_crawler/di/Config.kt
@@ -5,7 +5,7 @@ object Config {
     const val pathToChromeDriver: String = "chromedriver.exe"
     const val jdbcUrl = "jdbc:sqlite:.vk_photo_crawler.db"
     const val photosDir = "D:\\PicturesVk"
-    const val vkTimeout: Long = 3L
+    const val vkTimeout: Long = 3000L
     const val minDelayBetweenOpenPhotoPost: Long = 500L
     const val maxDelayBetweenOpenPhotoPost: Long = 1000L
     const val vkMorePostRequestUrl = "https://vk.com/al_wall.php"
